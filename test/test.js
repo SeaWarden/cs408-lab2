2490ddb29eeed26c5ce6e12a3fc9391887b57a0f
noahseward <noahseward@u.boisestate.edu>
2025-01-22 23:11:16 -0700

    Added tests for different number inputs

--- test/test.js
+++ test/test.js
@@ -4,14 +4,62 @@ import sinon from 'sinon';
 
 QUnit.module('main.js tests', function() {
 
-    QUnit.test('helloWorld should print Hello World to the console', function(assert) {
+    // Throws error because preview does not have a windows alert
+    QUnit.test('helloWorld should print Hello World as an alert', function(assert) {
         //Arrange
-        const consoleSpy = sinon.spy(console, 'log');
+        const alertSpy = sinon.spy(window, 'alert');
         //Act
         helloWorld();
         //Assert
-        assert.ok(consoleSpy.calledWith('Hello World'), 'console.log should be called with Hello World');
-        consoleSpy.restore();
+        assert.ok(alertSpy.calledWith('Hello World'), 'alert should be called with Hello World');
+        alertSpy.restore();
+    });
+
+    QUnit.test('add should throw an error if num1 is not provided', function(assert) {
+        //Arrange
+        const num1 = undefined;
+        const num2 = 3;
+        //Act
+        //Assert
+        assert.throws(() => add(num1, num2), 'You must provide two numbers to add');
+    });
+
+    QUnit.test('add should throw an error if num2 is not provided', function(assert) {  
+        //Arrange
+        const num1 = 2;
+        const num2 = undefined;
+        //Act
+        //Assert
+        assert.throws(() => add(num1, num2), 'You must provide two numbers to add');
+    });
+
+    QUnit.test('add should throw an error if num1 is not a number', function(assert) {
+        //Arrange
+        const num1 = '2';
+        const num2 = 3;
+        //Act
+        //Assert
+        assert.throws(() => add(num1, num2), 'You must provide two numbers to add');
+    });
+
+    QUnit.test('add should throw an error if num2 is not a number', function(assert) {
+        //Arrange
+        const num1 = 2;
+        const num2 = '3';
+        //Act
+        //Assert
+        assert.throws(() => add(num1, num2), 'You must provide two numbers to add');
+    });
+
+    QUnit.test('add should return 0 if both numbers are 0', function(assert) {
+        //Arrange
+        const num1 = 0;
+        const num2 = 0;
+        const expected = 0;
+        //Act
+        const result = add(num1, num2);
+        //Assert
+        assert.equal(result, expected, 'add(0, 0) should return 0');
     });
 
     QUnit.test('add should return the sum of two numbers', function(assert) {
